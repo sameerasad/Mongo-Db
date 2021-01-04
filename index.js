cf16ccdc28147e245f534416007a2307a5e9459e
sameerasad <ssameerkhan525@gmail.com>
2021-01-04 17:54:27 +0500

    create schema using mongoose.Schema method

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ mongoose
 
   .then(() => console.log("connected sucessfully"))
   .catch((err) => console.error("could not connect ", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
