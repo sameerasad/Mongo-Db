b7d75f12784d024822d2bf5d47ca369a59428eca
sameerasad <ssameerkhan525@gmail.com>
2021-01-07 19:29:59 +0500

    connection of with creation of data and retrival of data

--- exercise.js
+++ exercise.js
@@ -1,22 +1,43 @@
 const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/vidly1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected sucessfully"))
+  .catch((err) => console.error("could not connect ", err));
 
 const moviesSchema = new mongoose.Schema({
   name: String,
   price: Number,
-  category: String,
+  genre: String,
   availability: Boolean,
   date: { type: Date, default: Date.now },
 });
-const Movies = mongoose.model("vidly", moviesSchema);
+
+const Movies = mongoose.model("movies", moviesSchema);
 async function createMovies() {
   const movies = new Movies({
-    name: "harry potter",
-    price: 50,
-    category: "fiction",
+    name: "harry potter1",
+    price: 60,
+    genre: "fiction",
     availability: true,
   });
 
-  const result = await movies.save(movies);
+  const result = await movies.save();
 
   console.log(result);
 }
+
+async function getMovies() {
+  try {
+    const result = await Movies.find().and([
+      { name: "harry potter", price: 50 },
+    ]);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getMovies();
