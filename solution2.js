39cb88011fb7667ce742cd1cd7f0287a0a8ba22e
sameerasad <ssameerkhan525@gmail.com>
2021-01-13 21:28:21 +0500

    fetch all the front an backend course by decending price and display name and author

---
+++ solution2.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/exercise", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("mongoDb  connected sucessfully");
+  })
+  .catch((err) => console.error("could not connect"));
+
+const schema = mongoose.Schema({
+  tags: [String],
+  date: Date,
+  name: String,
+  author: String,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("courses", schema);
+
+async function getCourses() {
+  return await Course.find({
+    isPublished: true,
+    tags: { $in: ["frontend", "backend"] },
+  })
+    .sort("-price") //we could write it like {price:-1}in object form
+    .select(" name author"); //we could write it like {name:1,author:1cle}in object form
+}
+
+async function displayCourses() {
+  const result = await getCourses();
+  console.log(result);
+}
+
+displayCourses();
